3c424df641ba2d77e1601e8e9cc9a4dc1de6c206
AytanHuseynli <aytanhuseynli24@gmail.com>
2024-10-18 17:55:49 +0400

    Weapon

---
+++ Weapon/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace Weapon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Weapon weapon=new Weapon(30,10,"tekli");
+
+            bool cixis=false;
+            bool logout =false;
+            
+            do
+            {
+                Console.WriteLine("emeliyyat sec ");
+                Console.WriteLine("1: Ates acmaq ucun");
+                Console.WriteLine("2: Daraga lazim olan gulle sayi");
+                Console.WriteLine("3: Daragi yenilemek ucun");
+                Console.WriteLine("4: Ates modunu deyismek ucun");
+                Console.WriteLine("5: Proqrami dayandir");
+                Console.WriteLine("6: Yeni menyuya kec");
+                string secim=Console.ReadLine();
+                switch(secim)
+                {
+                    case "1":
+                        weapon.Shoot();
+                        break;  
+
+                    case "2":
+                        weapon.Getremainbullet();
+                        break;
+                    case "3":
+                        weapon.Changefiremod();
+                        break;
+                    case "4":
+                        weapon.Reload();
+                        break;
+                      case "5":
+                        Console.WriteLine("emeliyyatdan cixis edildi");
+                        return;
+                    case "6":
+                        
+                        
+                        do
+                        {
+                            Console.WriteLine("YENI MENYU ACILDI 7:Tutumu deyismek ucun sec,   8:gulle sayini deyismek ucun sec,    9 geri menyuya qayit");
+                            int cixisetmek;
+
+                           logout = int.TryParse(Console.ReadLine(),out cixisetmek);
+                           
+                            switch(cixisetmek)
+                            {
+                                case 7:
+                                    weapon.tutumdeyis();
+                                    break;
+                                case 8:
+                                    weapon.sayideyis();
+                                    break;
+                                case 9:
+                                    Console.WriteLine("menyudan cixis edildi");
+                                    cixis = true;
+                                    break;
+                            }
+
+                        }
+                        while (!cixis);
+
+                        break;
+                   
+                    case "0":
+                        Console.WriteLine($"tutum: {weapon.Tutum}  say:  {weapon.Say}  mod{weapon.Mod}");
+                        break;
+                     default:
+                        Console.WriteLine("DUZGUN SECIM ETMEMISIZ");
+                        break;
+
+                }
+            }
+            while(true);
+
+
+        }
+    }
+}
---
+++ Weapon/Weapon.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weapon
+{
+    public class Weapon
+    {
+        public int Tutum;
+        public int Say;
+        public string Mod;
+
+
+        public Weapon(int tutum,int say,string mod )
+        {
+            Tutum = tutum;
+            Say = say;  
+
+            Mod = mod;
+        }
+        public void Shoot()
+        {
+            if( Say == 0 )
+            {
+                Console.WriteLine("daraqda gulle yoxdur");
+                return;
+            }
+            if (Mod.ToLower() == "tekli")
+            {
+                Console.WriteLine("ates edildi");
+                Say--;
+                Console.WriteLine("GULLE SAYI  "+Say);
+                
+
+            }
+            else if(Mod.ToLower() == "avtomatik")
+            {
+                Console.WriteLine("ates edilir");
+                while ( Say > 0 )
+                {
+                    Say--;
+                    
+                }
+                Console.WriteLine("daraqda gulle bitdi "+Say);
+            }
+        }
+        public void Changefiremod()
+        {
+            if (Mod.ToLower() == "tekli")
+            {
+                Mod = "avtomatik";
+               
+                Console.WriteLine("atis modu avtomatik olaraq deyisdirildi");
+            }
+            else 
+            Console.WriteLine("mod tekli olraraq deyisdirildi");
+            Mod = "tekli";
+        }
+        public void  Getremainbullet()
+        {
+            Console.WriteLine("daragin dolmasi cucun lazim olan gulle sayi:  "+(Tutum- Say));
+            
+
+        }
+        public void Reload()
+        {
+            if (Say < Tutum)
+            {
+                int a;
+                a= Tutum - Say;
+                Say += a;
+                Console.WriteLine("daraq tam dolduruldu ,daraqdaki gulle sayi  "+Tutum);
+            }
+            else { Console.WriteLine("daraq tam doludu"); }
+        }
+        public void tutumdeyis()
+        {
+            Tutum = Tutum + 10;
+            Console.WriteLine("Silahinizin yeni tutumu "+Tutum);
+        }
+        public void sayideyis()
+        {
+            Say++;
+            Console.WriteLine("gulle sayi deyisdirilerek 1 vahid artirildi  "+Say);
+        }
+
+
+
+
+    }
+}
